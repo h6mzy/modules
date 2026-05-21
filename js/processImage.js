111e76dcba9eea4be85dab67696bcb14873f9d75
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-05-21 09:52:36 +0800

    Update processImage.js

--- js/processImage.js
+++ js/processImage.js
@@ -1,24 +1,35 @@
-async function processImage(file, {
-  width = 500,
-  type = 'image/webp',
-  quality = 0.6
-} = {}) {
+export default async function processImage(
+  file,
+  {
+    width = 500,
+    type = 'image/webp',
+    quality = 0.6
+  } = {}
+) {
   const bitmap = await createImageBitmap(file);
-  const scale = width / bitmap.width;
+
+  // prevent upscaling
+  const targetWidth = Math.min(width, bitmap.width);
+
+  const scale = targetWidth / bitmap.width;
+
   const canvas = document.createElement('canvas');
 
-  canvas.width = width;
+  canvas.width = targetWidth;
   canvas.height = Math.round(bitmap.height * scale);
 
   const ctx = canvas.getContext('2d');
 
   ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height);
 
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     canvas.toBlob(blob => {
+      if (!blob) {
+        reject(new Error('Image processing failed'));
+        return;
+      }
+
       resolve(blob);
     }, type, quality);
   });
 }
-
-export default processImage
