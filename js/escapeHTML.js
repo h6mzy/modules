76b1a02bfd3b1ed4450dc231875faaa2af4952ef
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-10 16:10:32 +0800

    Create escapeHTML.js

---
+++ js/escapeHTML.js
@@ -0,0 +1,8 @@
+export default function escapeHTML(str = '') {
+  return str
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
+}
