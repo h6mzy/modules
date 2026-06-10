4fa0232fce1bd5e6277e8f962d07973bf1590c27
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-10 16:20:50 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as countdown } from './countdown.js';
+export { default as Dialog } from './js/dialog.js';
+export { default as escapeHTML } from './js/escapeHTML.js';
+export { default as optimizeImage } from './js/optimizeImage.js';
+export { default as toast } from './js/toast.js';
