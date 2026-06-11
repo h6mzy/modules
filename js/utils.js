85ec5a3467cc278d32a0787464dfeb172e0c0458
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-11 13:25:34 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -64,3 +64,12 @@ export function startCountdown(target, { onTick, onEnd } = {}) {
 
   return () => clearInterval(id);
 }
+
+export function escapeHTML(str = '') {
+  return str
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
+}
