499f3aa777029423bfe6b244a2333f9c22f4369d
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-06-11 13:10:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 export { default as countdown } from './countdown.js';
-export { default as Dialog } from './js/dialog.js';
-export { default as escapeHTML } from './js/escapeHTML.js';
-export { default as optimizeImage } from './js/optimizeImage.js';
-export { default as toast } from './js/toast.js';
+export { default as Dialog } from './dialog.js';
+export { default as escapeHTML } from './escapeHTML.js';
+export { default as optimizeImage } from './optimizeImage.js';
+export { default as toast } from './toast.js';
